b5dbf4fdfb1c4a9416111ca0a66588010cccb76f
Farkinator <thefarkinator@gmail.com>
2015-09-16 16:49:16 -0400

    Created background layer. Waiting for BG resource.

--- markSeddon/src/BackgroundLayer.js
+++ markSeddon/src/BackgroundLayer.js
@@ -1,3 +1,20 @@
 /**
  * Created by Farkinator on 9/16/2015.
  */
+var BackgroundLayer = cc.Layer.extend({
+    ctor:function () {
+        this._super();
+        this.init();
+    },
+
+    init:function () {
+        this._super();
+        var winsize = cc.director.getWinSize();
+
+        //create the background image and position it at the center of screen
+        var centerPos = cc.p(winsize.width / 2, winsize.height / 2);
+        var spriteBG = new cc.Sprite(res.PlayBG_png);
+        spriteBG.setPosition(centerPos);
+        this.addChild(spriteBG);
+    }
+});
