3a46583c7a429e3fbf3ef4e938b1816a6fb78f42
Farkinator <thefarkinator@gmail.com>
2015-09-21 22:05:47 -0400

    Pulling art in

--- markSeddon/src/resource.js
+++ markSeddon/src/resource.js
@@ -2,17 +2,17 @@ var res = {
     helloBG_png : "res/SeddonTitle.png",
     start_n_png : "res/PlayButton.png",
     start_s_png : "res/PlayButton.png",
-    playBG_png  : "res/bg_plac.png",
+    playBG_png  : "res/BoardBack.png",
     CloseNormal_png : "res/CloseNormal.png",
     CloseSelected_png : "res/CloseSelected.png",
-    board_png: "res/board.png",
+    board_png: "res/Grid.png",
 
     block1_png: "res/BlockEntertain.png",
-    block2_png: "res/Block.png",
-    block3_png: "res/block3.png",
-    block4_png: "res/block4.png",
-    block5_png: "res/block5.png",
-    block6_png: "res/block6.png"
+    block2_png: "res/BlockPolitics.png",
+    block3_png: "res/BlockWorld.png",
+    block4_png: "res/BlockSports.png",
+    block5_png: "res/BlockTech.png",
+    block6_png: "res/BlockRotate.png"
 };
 
 var g_resources = [
